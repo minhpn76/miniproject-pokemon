de44b37f825c3eb30a96f7bba89ea7b1e7485cb7
minhpn <minhpn@tinhvan.com>
2023-01-01 11:20:11 +0700

    update

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -19,13 +19,13 @@ const Pagination = ({ page, handleChangePage }: PaginationProps) => {
       <button
         disabled={page === 0}
         onClick={() => changePage('PREV')}
-        className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        className="relative inline-flex items-center rounded-md text-white bg-rose-600 px-4 py-2 text-sm font-medium disabled:opacity-75 enabled:hover:text-gray-700 enabled:hover:bg-white"
       >
         Previous
       </button>
       <button
         onClick={() => changePage('NEXT')}
-        className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        className="relative ml-3 inline-flex items-center rounded-md text-white bg-rose-600 px-4 py-2 text-sm font-medium  hover:text-gray-700 hover:bg-white"
       >
         Next
       </button>
