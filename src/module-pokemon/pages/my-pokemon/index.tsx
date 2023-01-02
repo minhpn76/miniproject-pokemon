5f515fefd5a3e0a4c714777e7cf9637e6e50e04b
minhpn <minhpn@tinhvan.com>
2023-01-02 11:44:23 +0700

    icon

--- src/module-pokemon/pages/my-pokemon/index.tsx
+++ src/module-pokemon/pages/my-pokemon/index.tsx
@@ -55,9 +55,22 @@ const MyPokemon = () => {
                               onClick={() => {
                                 setOpenModal(true);
                               }}
-                              className="flex items-center justify-center border border-transparent rounded-md bg-rose-600 py-2 px-5 text-base font-medium text-white hover:bg-rose-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                              className="flex items-center justify-center border border-transparent rounded-md bg-rose-600 p-2 smS:p-[3px] text-base font-medium text-white hover:bg-rose-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                             >
-                              Release All
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                />
+                              </svg>
                             </button>
                           </th>
                         )}
@@ -68,8 +81,7 @@ const MyPokemon = () => {
                         <tr className="border-b cursor-pointer" key={JSON.stringify(item)}>
                           <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{idx + 1}</td>
                           <td
-                            className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                            width={'20%'}
+                            className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap w-[20%] smS:w-full"
                             onClick={() => navigateDetail(item.generalInformation.name)}
                           >
                             <div className="lazyload-wrapper flex">
@@ -91,9 +103,22 @@ const MyPokemon = () => {
                                 setOpenModal(true);
                                 setPokemonRelease(item.generalInformation.name);
                               }}
-                              className="w-2/4 flex items-center justify-center border border-transparent rounded-md bg-rose-600 py-2 px-5 text-base font-medium text-white hover:bg-rose-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                              className="flex items-center justify-center border border-transparent rounded-md bg-rose-600 p-2 smS:p-[3px] text-base font-medium text-white hover:bg-rose-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                             >
-                              Release
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                />
+                              </svg>
                             </button>
                           </td>
                         </tr>
